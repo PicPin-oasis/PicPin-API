c94bce174382f291f8ff429e978f855dbc694d8a
lob <coffeescript@kakao.com>
2023-11-06 00:52:30 +0900

    chore: ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,48 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
-	id("org.springframework.boot") version "3.1.5"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
-	kotlin("plugin.jpa") version "1.8.22"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.jpa") version "1.8.22"
 }
 
 group = "com.picpin"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.0.4")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.0.4")
-	implementation("com.auth0:java-jwt:4.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.0.4")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.0.4")
+    implementation("com.auth0:java-jwt:4.3.0")
 
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
-	runtimeOnly("com.mysql:mysql-connector-j:8.0.32")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
+    runtimeOnly("com.mysql:mysql-connector-j:8.0.32")
 
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
-	implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
