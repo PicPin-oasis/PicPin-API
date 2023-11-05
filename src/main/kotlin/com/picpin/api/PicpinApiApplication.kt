a49e62482b65989b7e9be71eeb88ac9b47de6207
Seokhyeon Kim <67903919+Lob-dev@users.noreply.github.com>
2023-11-06 00:53:02 +0900

    chore: ktlint (#6)

--- src/main/kotlin/com/picpin/api/PicpinApiApplication.kt
+++ src/main/kotlin/com/picpin/api/PicpinApiApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class PicpinApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<PicpinApiApplication>(*args)
+    runApplication<PicpinApiApplication>(*args)
 }
