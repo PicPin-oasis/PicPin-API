6a538cf95247f5ad9df6efcc5269199b0266becc
lob <coffeescript@kakao.com>
2023-11-07 20:07:58 +0900

    feat: add model for custom access token

--- src/main/kotlin/com/picpin/api/domain/oauth/TokenGenerator.kt
+++ src/main/kotlin/com/picpin/api/domain/oauth/TokenGenerator.kt
@@ -2,6 +2,7 @@ package com.picpin.api.domain.oauth
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.picpin.api.domain.oauth.model.CustomAccessToken
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
@@ -17,17 +18,19 @@ class TokenGenerator(
 ) {
     private val key = Algorithm.HMAC256(secret.toByteArray(StandardCharsets.UTF_8))
 
-    fun generateAccessToken(accountId: Long): String {
+    fun generateAccessToken(accountId: Long): CustomAccessToken {
         val accessExpireTime = LocalDateTime.now()
             .plusSeconds(accessTokenExpireTime)
             .atZone(ZoneId.systemDefault()).toInstant()
 
-        return JWT.create()
+        val payload = JWT.create()
             .withIssuer(issuer)
             .withJWTId(accountId.toString())
             .withClaim(SNS_TYPE_NAME, KAKAO_SNS_TYPE_VALUE)
             .withExpiresAt(Date.from(accessExpireTime))
             .sign(key)
+
+        return CustomAccessToken(payload, accessTokenExpireTime)
     }
 
     companion object {
