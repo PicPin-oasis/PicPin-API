9d9b83bd0e1bd7a412074503ea5b89c5134fdfbe
lob <coffeescript@kakao.com>
2023-11-07 22:57:47 +0900

    refactor: rename token components

--- src/main/kotlin/com/picpin/api/domain/oauth/AccessTokenGenerator.kt
+++ src/main/kotlin/com/picpin/api/domain/oauth/AccessTokenGenerator.kt
@@ -2,7 +2,7 @@ package com.picpin.api.domain.oauth
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.picpin.api.domain.oauth.model.CustomAccessToken
+import com.picpin.api.domain.oauth.model.GenerateAccessToken
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
@@ -11,14 +11,14 @@ import java.time.ZoneId
 import java.util.*
 
 @Component
-class TokenGenerator(
+class AccessTokenGenerator(
     @Value("\${jwt.issuer}") private val issuer: String,
     @Value("\${jwt.secret}") private val secret: String,
     @Value("\${jwt.access_token_expire_time}") private val accessTokenExpireTime: Long
 ) {
     private val key = Algorithm.HMAC256(secret.toByteArray(StandardCharsets.UTF_8))
 
-    fun generateAccessToken(accountId: Long): CustomAccessToken {
+    fun generate(accountId: Long): GenerateAccessToken {
         val accessExpireTime = LocalDateTime.now()
             .plusSeconds(accessTokenExpireTime)
             .atZone(ZoneId.systemDefault()).toInstant()
@@ -30,7 +30,7 @@ class TokenGenerator(
             .withExpiresAt(Date.from(accessExpireTime))
             .sign(key)
 
-        return CustomAccessToken(payload, accessTokenExpireTime)
+        return GenerateAccessToken(payload, accessTokenExpireTime)
     }
 
     companion object {
--- src/main/kotlin/com/picpin/api/usecases/KakaoSocialLoginUseCase.kt
+++ src/main/kotlin/com/picpin/api/usecases/KakaoSocialLoginUseCase.kt
@@ -1,9 +1,9 @@
 package com.picpin.api.usecases
 
 import com.picpin.api.domain.account.AccountService
+import com.picpin.api.domain.oauth.AccessTokenGenerator
 import com.picpin.api.domain.oauth.KakaoAccessTokenReader
 import com.picpin.api.domain.oauth.KakaoProfileReader
-import com.picpin.api.domain.oauth.TokenGenerator
 import com.picpin.api.domain.oauth.model.toAccount
 import com.picpin.api.interfaces.model.OAuthResponse
 import io.github.oshai.kotlinlogging.KLogger
@@ -15,7 +15,7 @@ class KakaoSocialLoginUseCase(
     private val kakaoAccessTokenReader: KakaoAccessTokenReader,
     private val kakaoProfileReader: KakaoProfileReader,
     private val accountService: AccountService,
-    private val tokenGenerator: TokenGenerator
+    private val tokenGenerator: AccessTokenGenerator
 ) {
     val logger: KLogger = KotlinLogging.logger { }
 
@@ -27,7 +27,7 @@ class KakaoSocialLoginUseCase(
         )
 
         val account = accountService.ifNotExistsSignUp(kakaoUserInfo.toAccount())
-        val accessToken = tokenGenerator.generateAccessToken(account.id!!)
+        val accessToken = tokenGenerator.generate(account.id!!)
 
         return OAuthResponse(accessToken, kakaoAccessToken.tokenType)
     }
