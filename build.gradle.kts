b24236fa91412b4a344cb9fd4de7b6a53bc9853c
Seokhyeon Kim <67903919+Lob-dev@users.noreply.github.com>
2023-11-07 23:32:20 +0900

    Develop (#34)
    * chore: make gradlew executable (#4)
    
    * chore: ktlint (#6)
    
    * chore: change env (#8)
    
    * chore: change server port (#10)
    
    * chore: change server port (#12)
    
    * chore: change server port (#14)
    
    * chore: change workflow (#16)
    
    * revert: push ecr (#19)
    
    * chore: PICPIN-API-21, 소셜 로그인 (#23)
    
    * chore: change environment name
    
    * feat: add account entity
    
    * feat: add kakao social login
    
    * feat: add api and use case
    
    * Update deploy_main.yml (#25)
    
    * feat: PICPIN-API-27, 소셜 로그인 시 자체 토큰을 응답하도록 변경 (#28)
    
    * refactor: rename & extract components
    
    * chore: change environment name
    
    * feat: generate custom token
    
    * feat: add token generator
    
    * feat: set cors
    
    * feat: change token to responsive
    
    * feat: add model for custom access token
    
    * Lob dev patch 12 (#30)
    
    * fix: compile error
    
    * fix: fix placeholder exception
    
    * feat: PICPIN-API-32, 사진 업로드 경로 이미지 가져오기 API 구현 (#33)
    
    * chore: add workflow for pull request build
    
    * refactor: rename token components
    
    * feat: add account resolve logics
    
    * feat: add config for s3
    
    * feat: add get upload url components
    
    * chore: change workflow to only ktlint check
    
    * fix: fix workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
 }
 
---
+++ src/main/kotlin/com/picpin/api/interfaces/PhotoApi.kt
@@ -0,0 +1,21 @@
+package com.picpin.api.interfaces
+
+import com.picpin.api.domain.photo.model.UploadUrlResponse
+import com.picpin.api.interfaces.model.GetUploadUrlRequest
+import com.picpin.api.usecases.GetUploadUrlUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PhotoApi(
+    private val getUploadUrlUseCase: GetUploadUrlUseCase
+) {
+
+    @PostMapping("/photos/upload-url")
+    fun getUploadUrl(@RequestBody request: GetUploadUrlRequest): ResponseEntity<UploadUrlResponse> {
+        val response = getUploadUrlUseCase.process(request.imageName)
+        return ResponseEntity.ok(response)
+    }
+}
