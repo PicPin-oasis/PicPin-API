71fd36c0c9cd72490abd91c11c298e9a4a18f237
lob <coffeescript@kakao.com>
2023-11-08 22:03:29 +0900

    feat: add get my profile use case

--- src/main/kotlin/com/picpin/api/domain/account/AccountService.kt
+++ src/main/kotlin/com/picpin/api/domain/account/AccountService.kt
@@ -21,4 +21,7 @@ class AccountService(
             accountRepository.save(account)
         }
     }
+
+    @Transactional(readOnly = true)
+    fun findBy(accountId: Long): Account = accountRepository.findBy(accountId)
 }
---
+++ src/main/kotlin/com/picpin/api/interfaces/AccountApi.kt
@@ -0,0 +1,22 @@
+package com.picpin.api.interfaces
+
+import com.picpin.api.aspect.AccountId
+import com.picpin.api.interfaces.model.MyProfileResponse
+import com.picpin.api.usecases.GetMyProfileUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/accounts")
+class AccountApi(
+    private val getMyProfileUseCase: GetMyProfileUseCase
+) {
+
+    @GetMapping("/my-profile")
+    fun getMyProfile(@AccountId accountId: Long): ResponseEntity<MyProfileResponse> {
+        val response = getMyProfileUseCase.process(accountId)
+        return ResponseEntity.ok(response)
+    }
+}
