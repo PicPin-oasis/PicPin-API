bf9b117a18c6a276b9c2b0342b2c8f8e8e4ba1c4
Seokhyeon Kim <67903919+Lob-dev@users.noreply.github.com>
2023-11-11 02:13:03 +0900

    Develop (#51)
    * chore: make gradlew executable (#4)
    
    * chore: ktlint (#6)
    
    * chore: change env (#8)
    
    * chore: change server port (#10)
    
    * chore: change server port (#12)
    
    * chore: change server port (#14)
    
    * chore: change workflow (#16)
    
    * revert: push ecr (#19)
    
    * chore: PICPIN-API-21, 소셜 로그인 (#23)
    
    * chore: change environment name
    
    * feat: add account entity
    
    * feat: add kakao social login
    
    * feat: add api and use case
    
    * Update deploy_main.yml (#25)
    
    * feat: PICPIN-API-27, 소셜 로그인 시 자체 토큰을 응답하도록 변경 (#28)
    
    * refactor: rename & extract components
    
    * chore: change environment name
    
    * feat: generate custom token
    
    * feat: add token generator
    
    * feat: set cors
    
    * feat: change token to responsive
    
    * feat: add model for custom access token
    
    * Lob dev patch 12 (#30)
    
    * fix: compile error
    
    * fix: fix placeholder exception
    
    * feat: PICPIN-API-32, 사진 업로드 경로 이미지 가져오기 API 구현 (#33)
    
    * chore: add workflow for pull request build
    
    * refactor: rename token components
    
    * feat: add account resolve logics
    
    * feat: add config for s3
    
    * feat: add get upload url components
    
    * chore: change workflow to only ktlint check
    
    * fix: fix workflow
    
    * feat: PICPIN-API-36, 사용자 정보 조회 API 구현 (#37)
    
    * chore: add test code for account service
    
    * refactor: modify cloud front host info to used only inside the S3 service
    
    * feat: add exclude pattern
    
    * feat: add get my profile use case
    
    * chore: change name GenerateAccessToken to JsonWebToken
    
    * refactor: Include refresh token in responses on social login
    
    * chore: change package name
    
    * chore: change package name
    
    * feat: add global exception handler
    
    * feat: apply custom exception's
    
    * chore: change package name
    
    * chore: run ktlint
    
    * feat: PICPIN-API-22, 포스트 생성, 수정 API 구현 (#39)
    
    * feat: add meta api
    
    * chore: modify fun to shared variable
    
    * chore: temp commit
    
    * chore: relocating base objects
    
    * chore: relocating base objects, change component name
    
    * feat: add post, post_coordinate domains
    
    * chore: rename models for social login
    
    * chore: rename models for social login
    
    * refactor: encapsulate access token extraction logics
    
    * feat: add write post use case, models
    
    * fix: change config for cors policy
    
    * chore: add http client env
    
    * chore: add http file for get my profile
    
    * chore: change model for exceptions
    
    * chore: set entity ids to non null
    
    * feat: modify post use case
    
    * chore: run ktlint
    
    * chore: run ktlint
    
    * chore: add .editorconfig
    
    * feat: add http file
    
    * fix: reverse condition
    
    * refactor: modify validations
    
    * refactor: change post columns
    
    * feat: write post with photos
    
    * feat: handle httpMessageNotReadableException
    
    * feat: modify post with photos
    
    * chore: run ktlint
    
    * feat: clean images to be deleted when modify post
    
    * feat: PICPIN-API-40, 앨범 조회, 생성, 앨범 미지정 포스트 목록 조회 API 구현 (#43)
    
    * feat: add get my albums use case
    
    * feat: add create my album use case
    
    * feat : add get my posts use case
    
    * feat: PICPIN-API-45, 포스트 날짜별 조회, 앨범별 조회 API 구현 (#46)
    
    * feat: add get my posts by date use case
    
    * feat: add get my posts by album use case
    
    * refactor: apply nested request models
    
    * feat: apply swagger
    
    * fix: compile error (#48)
    
    * fix: swagger config (#50)

--- src/main/kotlin/com/picpin/api/configuration/SwaggerConfiguration.kt
+++ src/main/kotlin/com/picpin/api/configuration/SwaggerConfiguration.kt
@@ -3,16 +3,29 @@ package com.picpin.api.configuration
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
 class SwaggerConfiguration {
 
+    @Profile("default")
     @Bean
-    fun openAPI(): OpenAPI = OpenAPI()
+    fun localOpenAPIConfig(): OpenAPI = OpenAPI()
         .components(Components())
         .info(getApiInfo())
+
+    @Profile("production")
+    @Bean
+    fun productionOpenAPIConfig(): OpenAPI {
+        val productionServer = Server().url("https://api.picpin.life")
+        return OpenAPI()
+            .components(Components())
+            .servers(listOf(productionServer))
+            .info(getApiInfo())
+    }
 
     private fun getApiInfo() = Info()
         .title("PICPIN API")
